b9a36a68f6b485724271b9d4baefe540cea2405a
Clive Crous <clive@crous.co.za>
2008-07-23 19:55:43 +0200

    Revert "remove some forgotten pieces of EXPORT_TILES"
    This reverts commit 8c5602ce735f3bb4c8dd5d6efd15172722ae0b50.
    
    Conflicts:

--- vultures/vultures_main.c
+++ vultures/vultures_main.c
@@ -41,6 +41,10 @@
 #include "vultures_mou.h"
 #include "vultures_opt.h"
 
+#ifdef EXPORT_TILES
+# include "vultures_conf.h"
+# include "vultures_export_tiles.h"
+#endif
 
 
 /* Interface definition, for windows.c */
@@ -174,6 +178,9 @@ void vultures_init_nhwindows(int *argcp, char **argv)
     iflags.menu_tab_sep = 1;
     iflags.wc_hilite_pet = 1;
 
+#ifdef EXPORT_TILES
+    vultures_export_tiles();
+#endif
 
     vultures_show_logo_screen();
 
